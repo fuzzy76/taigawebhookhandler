86d2f0a39ef30cffc22231a31909a25867369cba
Håvard Pedersen <fuzzy76@fuzzy76.net>
2015-06-19 15:03:43 +0200

    Change parsing, take 1

--- taiga.php
+++ taiga.php
@@ -30,12 +30,25 @@ class TaigaEvent { // http://taigaio.github.io/taiga-doc/dist/webhooks.html
     'issue' => 'issue',
     'wikipage' => 'wiki page'
   );
+  static $statusverb = array(
+    'New' => 'reset to new',
+    'Ready' => 'made ready',
+    'In progress' => 'started',
+    'Ready for test' => 'made available for testing',
+    'Done' => 'finished',
+    'Archived' => 'archived',
+    'Needs info' => 'requested info for',
+    'Closed' => 'closed',
+    'Rejected' => 'rejected',
+    'Postponed' => 'postponed'
+  );
 
 
   public function __construct($json) {
     $this->event = json_decode($json, TRUE);
     $this->objectname = ($this->event['type'] == 'wikipage') ? $this->event['data']['slug'] : $this->event['data']['subject'];
     $this->type = SELF::$typename[$this->event['type']];
+    $this->action_did = SELF::$verb[$this->event['action']];
     $this->expandChanges();
   }
 
@@ -43,7 +56,7 @@ class TaigaEvent { // http://taigaio.github.io/taiga-doc/dist/webhooks.html
 
     $what = ucfirst($this->type) . " '{$this->objectname}'";
 
-    $how = SELF::$verb[$this->event['action']];
+    $how = $this->action_did;
 
     $who = (isset($this->event['change']) ? $this->event['change']['user']['name'] : $this->event['data']['owner']['name']);
     $who = ($this->event['action'] == 'delete') ? "$who*" : $who;
@@ -53,7 +66,16 @@ class TaigaEvent { // http://taigaio.github.io/taiga-doc/dist/webhooks.html
   }
 
   function expandChanges() {
-    // TODO parse changed events to rename events to "closed", "opened". etc.
+    if ($this->event['action'] == 'change') {
+      $diff = $this->event['change']['diff'];
+      if ( (count($diff) == 1) && (isset($diff['status'])) ) {
+        $status_to = $this->event['change']['values']['status'][ $diff['status']['to'] ];
+        $this->action_did = $this->statusverb[$status_to];
+      } else {
+        $elems = implode(',', array_keys($diff));
+        $this->action_did = "edited ($elems)";
+      }
+    }
   }
 }
 
