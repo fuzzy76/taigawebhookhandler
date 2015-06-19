99c826c99e425a919b650b7cd90bc4474c9b91f5
Håvard Pedersen <fuzzy76@fuzzy76.net>
2015-06-19 13:32:23 +0200

    OO-oriented rewrite

--- taiga.php
+++ taiga.php
@@ -1,32 +1,69 @@
 <?php
 include "taiga_config.php";
 
-error_log('taiga notify start');
-$a = json_decode(file_get_contents('php://input'),TRUE);
-error_log('taiga notify ' . print_r($a,TRUE));
-$info = getInfo($a);
+$taigaevent = file_get_contents('php://input');
+error_log("taiga notify $taigaevent");
+$taigaevent = new TaigaEvent($taigaevent);
+$info = $taigaevent->getInfo();
 $msg = "[Taiga] $info";
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query(array('message'=>$msg)),
-    ),
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($conf['announce_url'], false, $context);
-error_log('taiga notify end');
-echo "script ended";
+post($conf['announce_url'], http_build_query(array('message'=>$msg)));
 
-function getInfo($a) {
-  $did = array('create' => 'created', 'delete' => 'deleted', 'change' => 'changed');
-  $b = "{$a['action']} {$a['type']}";
-  $who = (isset($a['change']) ? $a['change']['user']['name'] : $a['data']['owner']['name']);
-  $who = ($a['action'] == 'delete') ? "$who*" : $who;
-  $whatname = ($a['type'] == 'wikipage') ? $a['data']['slug'] : $a['data']['subject'];
-  $what = ucfirst($a['type']) . " '$whatname'";
-  $how = $did[$a['action']];
-  $msg = "$what $how by $who";
-  return $msg;
+class TaigaEvent { // http://taigaio.github.io/taiga-doc/dist/webhooks.html
+  // Source event
+  public $event = NULL;
+
+  // Calculated fields
+  public $objectname = '';
+  public $type = '';
+
+  // Lookup tables
+  static $verb = array(
+    'create' => 'created',
+    'delete' => 'deleted',
+    'change' => 'changed'
+  );
+  static $typename = array(
+    'test' => 'Testevent',
+    'milestone' => 'sprint',
+    'userstory' => 'user story',
+    'task' => 'task',
+    'issue' => 'issue',
+    'wikipage' => 'wiki page'
+  );
+
+
+  public function __construct($json) {
+    $this->event = json_decode($json, TRUE);
+    $this->objectname = ($this->event['type'] == 'wikipage') ? $this->event['data']['slug'] : $this->event['data']['subject'];
+    $this->type = SELF::$typename[$this->event['type']];
+    $this->expandChanges();
+  }
+
+  function getInfo() {
+
+    $what = ucfirst($this->type) . " '{$this->objectname}'";
+
+    $how = SELF::$verb[$this->event['action']];
+
+    $who = (isset($this->event['change']) ? $this->event['change']['user']['name'] : $this->event['data']['owner']['name']);
+    $who = ($this->event['action'] == 'delete') ? "$who*" : $who;
+
+    $msg = "$what $how by $who";
+    return $msg;
+  }
+
+  function expandChanges() {
+    // TODO parse changed events to rename events to "closed", "opened". etc.
+  }
+}
+
+function post ($url, $body) {
+  $options = array(
+      'http' => array( // use key 'http' even if you send the request to https://...
+          'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+          'method'  => 'POST',
+          'content' => $body,
+      ),
+  );
+  $result = file_get_contents($url, false, stream_context_create($options));
 }
